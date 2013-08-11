459bbf0d308c9d08c775c2966c420bdac192f881
rmoddel <rmoddel@gmail.com>
2013-08-11 01:06:47 -0400

    Adding the Transaction form

---
+++ transForm.php
@@ -0,0 +1,55 @@
+<?php  
+session_start();
+if (!isset($_SESSION['count']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+  $_SESSION['count'] = 0;
+} else {
+  $_SESSION['count']++;
+  $transaction = array();
+  $transaction['type'] = $_POST['trans_type'];
+  $transaction['amount'] = $_POST['amount'];
+  $_SESSION['transactions'][] = $transaction;
+}
+
+print_r($_SESSION);
+
+echo 'You accessed this page '. $_SESSION['count'] . 'times <br>';
+
+//session_destroy();
+
+
+$form = '
+<FORM action="form.php" method="post">
+    <fieldset>
+    <LABEL for="amount">Amount: </LABEL>
+    <INPUT type="text" name="amount" id="lastname"><BR>
+    <INPUT type="radio" name="trans_type" value="debt"> Debit<BR>
+    <INPUT type="radio" name="trans_type" value="credit"> Credit<BR>
+    <INPUT type="submit" value="Send"> <INPUT type="reset">
+    </fieldset>
+ </FORM>';
+
+  if($_SERVER['REQUEST_METHOD'] == 'GET') {
+     echo $form;
+  } else {
+
+     echo 'Thank you and this is what you sent: <br>';
+     foreach($_POST as $key => $value) {
+        echo $key . ': ' . $value . '<br>';
+     }   
+  }
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
